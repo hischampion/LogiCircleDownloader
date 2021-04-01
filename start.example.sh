4a7a0fd2a3a21de4e73f3ad3221d4963f9d6cef6
Scott Chapman <crionell@hotmail.com>
2021-03-31 20:43:24 -0700

    Making a default settings file

--- start.example.sh
+++ start.example.sh
@@ -4,5 +4,11 @@ export DOWNLOAD_DIRECTORY="downloads"
 
 export DEBUG="dsd"
 
-npx tsc | node built/downloader.js
+# if settings file does not exist, create default one
+# keeps your personal settings file out of git
+if ! test -f "settings.json"; then
+    cp settings.example.json settings.json
+fi
 
+npx tsc
+node built/downloader.js
